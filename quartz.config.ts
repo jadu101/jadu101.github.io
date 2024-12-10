c3d5daa003826d9d6b4e5c20e6ce6713f4424804
xh695r <miss4200miss@gmail.com>
2024-12-10 13:41:55 +0800

    Quartz sync: Dec 10, 2024, 1:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "👨‍💻My Notes",
+    pageTitle: "👨‍💻👾👨‍💻👾",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
